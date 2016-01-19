0fefca8f167fc5b03f7ba96832833460eaa7cb1f
Josh Wieder <josh.wieder@live.com>
2016-01-19 12:31:57 -0500

    PATH simplification
    simplified previous pull request to provide a solution to the PATH error
    in a single line rather than six.

--- S3-Filer-Mailer.sh
+++ S3-Filer-Mailer.sh
@@ -23,28 +23,18 @@
 ## Copyright (c) 2016, Josh Wieder
 ## All rights reserved.
 ####################################################
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+DIR=$(dirname "$(readlink -f "$0")")
 . $DIR/S3-Filer-Mailer.conf
-
 ROUTE=/tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/
 mkdir $ROUTE
 FILE=`mktemp $PREFIX.XXXXXX -p ${ROUTE}`
 TODAY=$(date +%Y-%m-%d)
 TIMESTAMP=$(date +%b_%e_%k:%M:%S)
 ATTACHMENT=notification.$(date +%s).tar.gz
-
 mkdir "$ROUTE"copies/
 mkdir "$ROUTE"press/
-
 printf "%s %s UID:[%s] PID:[%s] Reading messages ...\n" $TIMESTAMP $HOSTNAME $EUID $$ >> "$LOG"
 aws s3 ls $BUCKETURL$FOLDER | grep ${TODAY} > ${FILE}
-
 while read -r fileDate fileTime fileSize fileName
 do
         if [ "$fileDate" == "$TODAY" ]; then
@@ -52,9 +42,7 @@ do
                 aws s3 sync $BUCKETURL$FOLDER "$ROUTE"copies/ --exclude "*" --include "$fileName"
         fi
 done < "$FILE"
-
 TIMESTAMP=$(date +%b_%e_%k:%M:%S)
-
 msgCount=$(ls -1 "$ROUTE"copies/ | wc -l)
 if [ "$msgCount" -lt "1" ]; then
         printf "%s No daily contacts\n" $TIMESTAMP >> "$LOG"
@@ -65,7 +53,6 @@ else
 
         printf "%s Contact email sent with attachment\n" $TIMESTAMP >> "$LOG"
 fi
-
 rm -rf $ROUTE
 printf "S3 mailer exited with status code %s\n" $? >> "$LOG"
 exit
